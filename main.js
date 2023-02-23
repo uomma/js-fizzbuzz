632a3b07cf37bce34a5c9309b35e40644a52ffff
uomma <marcelloleone975@gmail.com>
2023-02-23 17:52:33 +0100

    funzoinante

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ for (let i = 1; i <= 100; i++) {
     }
     else if (i % 5 == 0) {
         console.log('buzz');
-    }
-    console.log(i)
+    }else {
+        console.log(i);
+      }
 
 } 
 /* ora stmpa anche frizzbuzz ma non sostituisce */
