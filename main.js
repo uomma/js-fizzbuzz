95844a6adfb6c5a5d78e4575fbed7f7e5d28c762
uomma <marcelloleone975@gmail.com>
2023-02-23 17:02:12 +0100

    stampa ma non sostituisce

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 'use: script'
 
-for(let i=1; i > 100; i++){
-    console.log(i)
-    if(i % 2 == 0){
-        console.log('pari')
+    /* for(let i=9; i >= 5; i++){
+        console.log(i);
+    }
+     */
+for (let i = 1; i <= 100; i++ ) {
+console.log(i)
+    if(i % 3 == 0){
+        console.log('frizz');
+    }
+    else if(i % 5 == 0){
+        console.log('buzz');
+    }
+    else(i % 15 == 0)
+    {
+    console.log('frizzbuzz');
     }
 }
