b3dde57eaffd32095549afdd0dcfba307ea270f5
uomma <marcelloleone975@gmail.com>
2023-02-23 17:38:15 +0100

    spostato console.log e corretto 2nd else if

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 'use: script'
 
-    /* for(let i=9; i >= 5; i++){
-        console.log(i);
-    }
-     */
-for (let i = 1; i <= 100; i++ ) {
-console.log(i)
-    if(i % 3 == 0){
+/* for(let i=9; i >= 5; i++){
+    console.log(i);
+}
+ */
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
         console.log('frizz');
     }
-    else if(i % 5 == 0){
+    else if (i % 5 == 0) {
         console.log('buzz');
     }
-    else(i % 15 == 0)
-    {
-    console.log('frizzbuzz');
+    else if (i % 15 == 0) {
+        console.log('frizzbuzz');
     }
-}
+    console.log(i)
+
+} 
