27d45004c9efd369a8a43eafcd979f881e3f871c
uomma <marcelloleone975@gmail.com>
2023-02-23 17:46:46 +0100

    avevo invertito frizz e buzz

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ for (let i = 1; i <= 100; i++) {
         console.log('frizzbuzz');
     }
     else if (i % 3 == 0) {
-        console.log('buzz');
+        console.log('frizz');
     }
     else if (i % 5 == 0) {
-        console.log('frizz');
+        console.log('buzz');
     }
     console.log(i)
 
 } 
+/* ora stmpa anche frizzbuzz ma non sostituisce */
